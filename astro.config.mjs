7201ced9c80b82fa196facc164fcf80e31e4e1a3
Ildefonso Lares <65493114+Flayxamax@users.noreply.github.com>
2025-07-23 17:25:55 -0700

    feat: Implement Service4 and Service5 component and minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import tunnel from "astro-tunnel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [tunnel()],
 });
